59d15bbfb03a351291f13cafc0b87c0ccafa0a06
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2024-11-28 11:00:14 -0500

    123

---
+++ inc/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Inicio - Sistema de Inventario</title>
+    <link rel="stylesheet" href="css/estilos.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <nav
+      class="navbar navbar-expand-lg"
+      style="
+        background-color: #0f67f6;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      "
+    >
+      <div class="container-fluid">
+        <!-- Brand -->
+        <a class="navbar-brand" href="index.html">
+          <img src="./img/logo_tecno.png" alt="Logo" width="65" height="28" />
+        </a>
+        <a class="navbar-brand text-white" href="#" style="font-weight: bold"
+          >Sistema de Inventario</a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarMenu"
+          aria-controls="navbarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style="color: #fff"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link text-white"
+                href="index.html"
+                style="opacity: 0.9"
+                >Inicio</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link text-white"
+                href="inventarios.html"
+                style="opacity: 0.9"
+                >Inventarios</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link text-white"
+                href="categorias.html"
+                style="opacity: 0.9"
+                >Categorías</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link text-white"
+                href="estudiantes.html"
+                style="opacity: 0.9"
+                >Estudiantes</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link text-white"
+                href="usuarios.html"
+                style="opacity: 0.9"
+                >Usuarios</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,38 +1,157 @@
-<?php require "./inc/session_start.php"; ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <?php include "./inc/head.php"; ?>
-    </head>
-    <body>
-        <?php
+<?php 
+include_once 'inc/header.php'
+?>
+    <!-- Encabezado -->
+    <div
+      class="d-flex justify-content-center align-items-center module-section"
+    >
+      <div class="text-center p-4 m-4 mb-1 border rounded shadow bg-white">
+        <h3 class="module-header">Módulos Profesionales</h3>
+      </div>
+    </div>
 
-            if(!isset($_GET['vista']) || $_GET['vista']==""){
-                $_GET['vista']="login";
-            }
+    <!-- Contenido Principal -->
+    <div class="container py-5">
+      <div class="row g-4">
+        <!-- Tarjeta 1 -->
+        <a class="col-lg-4 col-md-6" href="inventario.html">
+          <div class="card">
+            <img
+              src="https://via.placeholder.com/300"
+              class="card-img-top"
+              alt="Módulo 1"
+            />
+            <div class="card-body text-center">
+              <div class="position-relative mb-3">
+                <span
+                  class="badge rounded-circle position-absolute top-0 start-50 translate-middle text-white"
+                >
+                  1
+                </span>
+              </div>
+              <h5 class="mt-5 card-title">ANÁLISIS Y DISEÑO DE SISTEMAS WEB</h5>
+              <p class="card-text">
+                Construir programas de sistemas de información según el diseño
+                funcional, estándares TI, buenas prácticas de programación y
+                políticas de seguridad.
+              </p>
+            </div>
+          </div>
+        </a>
 
+        <!-- Tarjeta 2 -->
+        <a class="col-lg-4 col-md-6" href="inventario.html">
+          <div class="card">
+            <img
+              src="https://via.placeholder.com/300"
+              class="card-img-top"
+              alt="Módulo 2"
+            />
+            <div class="card-body text-center">
+              <div class="position-relative mb-3">
+                <span
+                  class="badge rounded-circle position-absolute top-0 start-50 translate-middle text-white"
+                >
+                  2
+                </span>
+              </div>
+              <h5 class="mt-5 card-title">DESARROLLO DE APLICACIONES WEB</h5>
+              <p class="card-text">
+                Realizar pruebas integrales de sistemas de información en la
+                fase de implantación, según estándares y buenas prácticas TI.
+              </p>
+            </div>
+          </div>
+        </a>
 
-            if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+        <!-- Tarjeta 3 -->
+        <a class="col-lg-4 col-md-6" href="inventario.html">
+          <div class="card">
+            <img
+              src="https://via.placeholder.com/300"
+              class="card-img-top"
+              alt="Módulo 3"
+            />
+            <div class="card-body text-center">
+              <div class="position-relative mb-3">
+                <span
+                  class="badge rounded-circle position-absolute top-0 start-50 translate-middle text-white"
+                >
+                  3
+                </span>
+              </div>
+              <h5 class="mt-5 card-title">DISEÑO DE SERVICIOS WEB</h5>
+              <p class="card-text">
+                Diseñar la presentación, navegación y organización de contenidos
+                web, asegurando usabilidad y experiencia de usuario óptimas.
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <!-- Columna 1 -->
+          <div
+            class="col-lg-3 col-md-6 mb-4 footer-logo text-center text-md-start"
+          >
+            <img
+              src="img/logo_tecno.png"
+              alt="Logo Institucional"
+              class="mb-3"
+            />
+            <p>Instituto de Educación Superior Tecnológico “Huanta”</p>
+            <a href="#" class="btn btn-primary btn-sm px-3">Sobre Nosotros</a>
+          </div>
 
-                /*== Cerrar sesion ==*/
-                if((!isset($_SESSION['id']) || $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario']=="")){
-                    include "./vistas/logout.php";
-                    exit();
-                }
+          <!-- Columna 2 -->
+          <div
+            class="col-lg-3 col-md-6 mb-4 footer-social text-center text-md-start"
+          >
+            <h5 class="footer-title">Facebook</h5>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+          </div>
 
-                include "./inc/navbar.php";
+          <!-- Columna 3 -->
+          <div
+            class="col-lg-3 col-md-6 mb-4 footer-contact text-center text-md-start"
+          >
+            <h5 class="footer-title">Información</h5>
+            <p>
+              <i class="fas fa-map-marker-alt"></i> Jr. Gral Córdova 650, Huanta
+              - Ayacucho
+            </p>
+            <p><i class="fas fa-phone"></i> Teléfono: (066) 322296</p>
+            <p>
+              <i class="far fa-clock"></i> Horario de Atención: <br />Lun -
+              Vier: 8 am - 1 pm (Mañana) <br />Lun - Vier: 3 pm - 6 pm (Tarde)
+            </p>
+          </div>
 
-                include "./vistas/".$_GET['vista'].".php";
+          <!-- Columna 4 -->
+          <div
+            class="col-lg-3 col-md-6 mb-4 footer-programs text-center text-md-start"
+          >
+            <h5 class="footer-title">Programas de estudios</h5>
+            <div>
+              <img src="img/PE1.png" alt="Programa 1" />
+              <img src="img/PE2.png" alt="Programa 2" />
+              <img src="img/PE3.png" alt="Programa 3" />
+              <img src="img/PE4.png" alt="Programa 4" />
+              <img src="img/PE5.png" alt="Programa 5" />
+            </div>
+          </div>
+        </div>
 
-                include "./inc/script.php";
+        <div class="footer-bottom">
+          © 2024 Instituto de Educación Superior Tecnológico “Huanta”
+        </div>
+      </div>
+    </footer>
 
-            }else{
-                if($_GET['vista']=="login"){
-                    include "./vistas/login.php";
-                }else{
-                    include "./vistas/404.php";
-                }
-            }
-        ?>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
